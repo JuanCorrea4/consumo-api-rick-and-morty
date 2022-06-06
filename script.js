511fce7fed3bfd2f0cdcd758ce4238fc553ae8ac
Juan Correa <102707755+JuanCorrea4@users.noreply.github.com>
2022-06-06 15:30:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 var div_user = document.querySelector("#container");
 
 var div_modal = document.querySelector("#modalWindow");
-function exit(){
+
+function exit() {
     document.getElementById("modalWindow").style.display = ("none");
 }
-function openTarget(id, name, gender, status, species,created) { 
+function openTarget(id, name, gender, status, species, created) {
     document.getElementById("modalWindow").style.display = ("block");
     let modal = document.createElement('div');
     div_modal.innerHTML = ""
     modal.innerHTML =
-            `<p>`+`ID #` + id+ `</p>`
-            + `<img src='https://rickandmortyapi.com/api/character/avatar/` + id + `.jpeg'>`
-            + `<p> ` + `Name: <b>` + name + `</b>`+ `</p>`
-            + `<p>`+ `Status: <b>` + status + `</b>` + `</p>`
-            + `<p>`+ `Gender: <b>` + gender + `<b>`+ `</p>`
-            + `<p>`+ `Especie: <b>` + species + `<b>`+ `</p>`
-            + `<p>`+ `Fecha de creacion: <b>` + created+ `<b>`+ `</p>`
-            +`<input id="buttonExit" type="button" value="Exit" onclick="exit()">`;
+        `<p>` + `ID #` + id + `</p>`
+        + `<img src='https://rickandmortyapi.com/api/character/avatar/` + id + `.jpeg'>`
+        + `<p> ` + `Name: <b>` + name + `</b>` + `</p>`
+        + `<p>` + `Status: <b>` + status + `</b>` + `</p>`
+        + `<p>` + `Gender: <b>` + gender + `<b>` + `</p>`
+        + `<p>` + `Especie: <b>` + species + `<b>` + `</p>`
+        + `<p>` + `Fecha de creacion: <b>` + created + `<b>` + `</p>`
+        + `<input id="buttonExit" type="button" value="Exit" onclick="exit()">`;
     div_modal.appendChild(modal)
 };
 
@@ -27,15 +28,11 @@ fetch(`https://rickandmortyapi.com/api/character`)
             let target = document.createElement('div');
             target.innerHTML =
                 `<section onclick="openTarget(${data.id}, '${data.name}', '${data.gender}', '${data.status}','${data.species}','${data.created}')">`
-                    + `<img src='https://rickandmortyapi.com/api/character/avatar/` + data.id + `.jpeg'>`
-                    + `<p> ` + `Name: <b>` + data.name + `</b>`+ `</p>`
-                    + `</section>`;
+                + `<img src='https://rickandmortyapi.com/api/character/avatar/` + data.id + `.jpeg'>`
+                + `<p> ` + `Name: <b>` + data.name + `</b>` + `</p>`
+                + `</section>`;
             div_user.appendChild(target)
 
         });
-        
+
     });
-
-
-
-
